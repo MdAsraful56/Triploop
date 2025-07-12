74dbbae0dc21966c74b7f39c0c2843b0390f79d6
MdAsraful56 <work.mdasraful56@gmail.com>
2025-07-12 10:06:50 +0600

    Module - 26 completed

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,26 +1,26 @@
-import { NextFunction, Request, Response } from "express";
-import { envVars } from "../config/env";
-import AppError from "../errorHelpers/AppError";
+import { NextFunction, Request, Response } from 'express';
+import { envVars } from '../config/env';
+import AppError from '../errorHelpers/AppError';
 
 export const globalErrorHandler = (
-  err: Error,
-  req: Request,
-  res: Response,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  next: NextFunction
+    err: Error,
+    req: Request,
+    res: Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    next: NextFunction
 ) => {
-  let statusCode = 500;
-  let message = `Something went wrong: ${err.message}`;
+    let statusCode = 500;
+    let message = `Something went wrong: ${err.message}`;
 
-  if (err instanceof AppError) {
-    statusCode = err.statusCode;
-    message = err.message;
-  }
+    if (err instanceof AppError) {
+        statusCode = err.statusCode;
+        message = err.message;
+    }
 
-  res.status(statusCode).json({
-    success: false,
-    message,
-    error: err,
-    stack: envVars.NODE_ENV === "development" ? err.stack : null,
-  });
+    res.status(statusCode).json({
+        success: false,
+        message,
+        error: err,
+        stack: envVars.NODE_ENV === 'development' ? err.stack : null,
+    });
 };
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,18 +1,64 @@
 import { NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status-codes';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
 import { UserService } from './user.service';
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-    try {
+// create user using try catch
+
+// const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//         const user = await UserService.createUser(req.body);
+//         res.status(201).json({
+//             message: 'User created Successful',
+//             user,
+//         });
+//     } catch (error) {
+//         next(error);
+//     }
+// };
+
+const createUser = catchAsync(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async (req: Request, res: Response, next: NextFunction) => {
         const user = await UserService.createUser(req.body);
-        res.status(201).json({
-            message: 'User created Successful',
-            user,
+
+        // res.status(httpStatus.CREATED).json({
+        //     message: 'User Create Successfully',
+        //     user,
+        // });
+
+        sendResponse(res, {
+            success: true,
+            statusCode: httpStatus.CREATED,
+            message: 'User Create Successfully',
+            data: user,
         });
-    } catch (error) {
-        next(error);
     }
-};
+);
+
+const getAllUsers = catchAsync(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async (req: Request, res: Response, next: NextFunction) => {
+        const result = await UserService.getAllUsers();
+
+        // res.status(httpStatus.OK).json({
+        //     success: true,
+        //     message: 'All User get Successfully',
+        //     users,
+        // });
+
+        sendResponse(res, {
+            success: true,
+            statusCode: httpStatus.CREATED,
+            message: 'User Create Successfully',
+            data: result.data,
+            meta: result.meta,
+        });
+    }
+);
 
 export const UserControllers = {
     createUser,
+    getAllUsers,
 };
