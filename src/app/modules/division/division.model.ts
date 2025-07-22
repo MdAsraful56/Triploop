5c9e1ad4f0d08da3e0c0c9760385c58163f6f37c
MdAsraful56 <work.mdasraful56@gmail.com>
2025-07-23 00:25:36 +0600

    tour & division type interface , schema create

---
+++ src/app/modules/division/division.model.ts
@@ -0,0 +1,16 @@
+import { model, Schema } from 'mongoose';
+import { IDevision } from './division.interface';
+
+const divisionSchema = new Schema<IDevision>(
+    {
+        name: { type: String, required: true, unique: true },
+        slug: { type: String, required: true, unique: true },
+        thumbnail: { type: String },
+        description: { type: String },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const Division = model<IDevision>('Division', divisionSchema);
