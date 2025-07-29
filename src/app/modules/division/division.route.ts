0c29f8a9c1e55eec1eda18077ac6655bd1358983
MdAsraful56 <work.mdasraful56@gmail.com>
2025-07-29 20:42:54 +0600

    division all completed

--- src/app/modules/division/division.route.ts
+++ src/app/modules/division/division.route.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { checkAuth } from '../../middlewares/checkAuth';
+
 import validateRequst from '../../middlewares/validateRequst';
 import { Role } from '../user/user.interface';
-import { DivisionControllers } from './division.controller';
-import { createDivisionSchema } from './division.validation';
+import { DivisionController } from './division.controller';
+import {
+    createDivisionSchema,
+    updateDivisionSchema,
+} from './division.validation';
 
 const router = Router();
 
@@ -11,7 +15,20 @@ router.post(
     '/create',
     checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
     validateRequst(createDivisionSchema),
-    DivisionControllers.createDivision
+    DivisionController.createDivision
+);
+router.get('/', DivisionController.getAllDivisions);
+router.get('/:slug', DivisionController.getSingleDivision);
+router.patch(
+    '/:id',
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    validateRequst(updateDivisionSchema),
+    DivisionController.updateDivision
+);
+router.delete(
+    '/:id',
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    DivisionController.deleteDivision
 );
 
 export const DivisionRoutes = router;
